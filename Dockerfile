88151f6f7a0054b4e75a8d442a2a2a86b49f99bd
Erik Damrose <damrose@univention.de>
2020-04-06 11:07:39 +0200

    Bug #49983: Remove loginpage, further adapt metadata
    Remove ucs login page, not longer needed with SAML login
    fix docker image upload
    Adapt app metadata and container env to current konnect version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 # hadolint ignore=DL3007
 FROM kopano/kopano_konnect:latest
 
-COPY login/identifier/build /var/lib/konnectd-docker/identifier-univention
+# COPY login/identifier/build /var/lib/konnectd-docker/identifier-univention
--- build-docker-container.sh
+++ build-docker-container.sh
@@ -4,20 +4,8 @@ set -xe
 
 DOCKER_IMG_VERSION=${DOCKER_IMG_VERSION:-latest}
 
-# build identifier app
-srcimg="debian:stretch"
-
-docker run \
-	--rm \
-	--volume "$(pwd)/login/":/src/login \
-	--entrypoint /src/login/build-identifier-docker.sh  \
-	"$srcimg" /src/login/identifier
-
-# build openid-connect-provider docker image
-if [ ! -e login/identifier/build ]; then
-	echo "No build directory, build failed"
-	exit 1
-fi
+# make sure the latest image is used locally
+docker pull $(cat Dockerfile | grep FROM | awk '{print $2}')
 
 docker build \
 	--force-rm \
@@ -25,5 +13,5 @@ docker build \
 	-t openid-connect-provider:$DOCKER_IMG_VERSION \
 	./
 
-docker tag openid-connect-provider:$DOCKER_IMG_VERSION docker-test.software-univention.de/openid-connect-provider:$DOCKER_IMG_VERSION
-docker push docker-test.software-univention.de/openid-connect-provider:$DOCKER_IMG_VERSION
+docker tag openid-connect-provider:$DOCKER_IMG_VERSION docker-test-upload.software-univention.de/openid-connect-provider:$DOCKER_IMG_VERSION
+docker push docker-test-upload.software-univention.de/openid-connect-provider:$DOCKER_IMG_VERSION
